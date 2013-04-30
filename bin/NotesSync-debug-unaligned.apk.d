2e15f85737fec5affb07106ccac495fba4d80847
Geetha Balasundaram <geetha.balasundaram@gmail.com>
2013-04-30 16:53:09 +0100

    adding latest files

--- bin/NotesSync-debug-unaligned.apk.d
+++ bin/NotesSync-debug-unaligned.apk.d
@@ -1,3 +1,4 @@
 /Users/tw-uk/IdeaProjects/NotesSync/bin/NotesSync-debug-unaligned.apk : \
 /Users/tw-uk/IdeaProjects/NotesSync/bin/NotesSync.ap_ \
 /Users/tw-uk/IdeaProjects/NotesSync/bin/classes.dex \
+/Users/tw-uk/IdeaProjects/NotesSync/libs/android-support-v4.jar \
--- bin/NotesSync.ap_.d
+++ bin/NotesSync.ap_.d
@@ -3,7 +3,9 @@
 /Users/tw-uk/IdeaProjects/NotesSync/res/drawable-ldpi/ic_launcher.png \
 /Users/tw-uk/IdeaProjects/NotesSync/res/drawable-mdpi/ic_launcher.png \
 /Users/tw-uk/IdeaProjects/NotesSync/res/drawable-xhdpi/ic_launcher.png \
+/Users/tw-uk/IdeaProjects/NotesSync/res/layout/create_button_fragment.xml \
 /Users/tw-uk/IdeaProjects/NotesSync/res/layout/main.xml \
+/Users/tw-uk/IdeaProjects/NotesSync/res/layout/note_text_fragment.xml \
 /Users/tw-uk/IdeaProjects/NotesSync/res/values/strings.xml \
 /Users/tw-uk/IdeaProjects/NotesSync/bin/res/drawable-hdpi/ic_launcher.png \
 /Users/tw-uk/IdeaProjects/NotesSync/bin/res/drawable-ldpi/ic_launcher.png \
