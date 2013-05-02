a8ae7178a1029398a86ea797d0aeb3b7fd1113e0
Geetha Balasundaram <geetha.balasundaram@gmail.com>
2013-05-02 13:21:50 +0100

    Stored notes in files - internal storage

--- src/com/notessync/activity/DisplayMessageActivity.java
+++ src/com/notessync/activity/DisplayMessageActivity.java
@@ -4,17 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+
 public class DisplayMessageActivity extends Activity {
 
     @SuppressWarnings("NewApi")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String note = (String) getIntent().getExtras().get(MyActivity.EXTRA_NOTE);
+//        String note = (String) getIntent().getExtras().get(MyActivity.EXTRA_NOTE);
+        StringBuilder notes = new StringBuilder();
+        try {
+            FileInputStream fileInputStream = openFileInput("notes-sync");
+            int content;
+            while ((content = fileInputStream.read()) != -1){
+                notes.append((char) content);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(note);
+        textView.setText(notes);
 
         setContentView(textView);
     }
--- src/com/notessync/activity/MyActivity.java
+++ src/com/notessync/activity/MyActivity.java
@@ -1,11 +1,14 @@
 package com.notessync.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import com.notessync.R;
+
+import java.io.FileOutputStream;
 
 public class MyActivity extends Activity {
 
@@ -20,10 +23,19 @@ public class MyActivity extends Activity {
         setContentView(R.layout.main);
     }
 
-    public void createNote(View view){
+    public void createNote(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.welcome_message);
-        intent.putExtra(EXTRA_NOTE,editText.getText().toString());
+
+//        intent.putExtra(EXTRA_NOTE,editText.getText().toString());
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("notes-sync", Context.MODE_APPEND);
+            String newNote = editText.getText().toString() + "\n";
+            fileOutputStream.write(newNote.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         startActivity(intent);
     }
 }
